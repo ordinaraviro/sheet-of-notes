63aece87b6cad0d873c26818716e446b43b749cf
ordinaraviro <94953757+ordinaraviro@users.noreply.github.com>
2022-07-16 11:07:38 +0300

    feat: add note template

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Sheet of notes</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
-
 const note = {
   add() {
     let newLi = document.createElement("li");
